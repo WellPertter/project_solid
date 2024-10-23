4f2600083720dccc2c88c3283331e28531ae768e
Lopes <josearthur.12311@gmail.com>
2024-10-22 21:04:28 -0300

    Add the control of screen using SOLID

---
+++ view/untScreen.pas
@@ -0,0 +1,24 @@
+unit untScreen;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  Tfrm2 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frm2: Tfrm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
